4fce22ce07c71b4cec36a085294a75e3db9261d1
Alexis <AlexDdevs@gmail.com>
2024-02-03 19:30:36 +0100

    Create Header and his css

---
+++ Portfolio/src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header className="header">
+            <div className="header-logo">
+                <span className="initial">AD</span>
+            </div>
+            <nav className="navbar">
+                <ul className="nav-list">
+                    <li className="nav-li">
+                        <a href="#about">À propos</a>
+                    </li>
+                    <li className="nav-li">
+                        <a href="#skills">Compétences</a>
+                    </li>
+                    <li className="nav-li">
+                        <a href="#projects">Projets</a>
+                    </li>
+                    <li className="nav-li">
+                        <a href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
