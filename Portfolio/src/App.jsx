b263635f3da74ad82d8febbe12d988fb42bb2d9c
Alexis <AlexDdevs@gmail.com>
2024-02-03 19:41:49 +0100

    Call Header and About to have a visualisation

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react"
+import Header from "./components/Header"
+import About from "./components/About"
+
 
 function App() {
-  return (
-    <>
-      
-    </>
-  )
+    return (
+        <>
+            <Header />
+            <About />
+        </>
+    )
 }
 
 export default App
