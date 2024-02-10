92cb2ad97f889617400f53ca54ac58a7db9ba613
Alexis <AlexDdevs@gmail.com>
2024-02-10 09:32:36 +0100

    Call Skills and create a section to call Projects

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,13 +1,31 @@
 import React from "react"
 import Header from "./components/Header"
 import About from "./components/About"
-
+import Skills from "./components/Skills"
+import Projects from "./components/Projects"
+import datas from "./data/dataProject.json"
 
 function App() {
     return (
         <>
             <Header />
             <About />
+            <Skills />
+            <section id="projects">
+                <h2 className="projects">Projets</h2>
+                <div className="container">
+                    {datas.map((item, i) => (
+                        <Projects
+                            key={i}
+                            title={item.title}
+                            description={item.description}
+                            repo={item.repo}
+                            site={item.website}
+                            techno={item.techno}
+                        />
+                    ))}
+                </div>
+            </section>
         </>
     )
 }
