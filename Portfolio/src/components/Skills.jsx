feef5a7de5be69ab9b6bdac18d023c38812c597b
Alexis <AlexDdevs@gmail.com>
2024-02-11 09:18:12 +0100

    delete const and add component for qualities and style section

--- Portfolio/src/components/Skills.jsx
+++ Portfolio/src/components/Skills.jsx
@@ -1,45 +1,34 @@
 import React from 'react'
 import SkillsItem from "./SkillsItem"
+import QualiItem from './QualiItem'
+import dataSkills from "../data/dataSkills.json"
 
 export default function Skills() {
-    const technologies = [
-        {
-            "techno": "fa-brands fa-html5",
-        },
-        {
-            "techno": "fa-brands fa-css3-alt",
-        },
-        {
-            "techno": "fa-brands fa-sass",
-        },
-        {
-            "techno": "fa-brands fa-square-js",
-        },
-        {
-            "techno": "fa-brands fa-react",
-        },
-        {
-            "techno": "fa-brands fa-github",
-        },
-        {
-            "techno": "ps",
-            "content": "Ps"
-        }
-    ]
-
     return (
         <section id="skills">
             <h2 className="skills">Compétences</h2>
-            <div className="techno-container">
-                <ul className="techno-list">
-                    {technologies.map((item, i) => (
-                        <SkillsItem
+            <div className="skills-container">
+                <div className="techno-container">
+                    <ul className="techno-list">
+                        {dataSkills.tech.map((item, i) => (
+                            <SkillsItem
                             key={i}
                             icon={item.techno}
                             content={item.content}
-                        />
-                    ))}
-                </ul>
+                            />
+                            ))}
+                    </ul>
+                </div>
+                <div className="more-container">
+                    <ul className="additionnal-skills">
+                        {dataSkills.qualities.map((quality, i) => (
+                            <QualiItem
+                                key={i}
+                                quality={quality}
+                            />
+                        ))}
+                    </ul>
+                </div>
             </div>
         </section>
     )
